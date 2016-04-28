8bdc96ce692271e908805c6607c0f739c6ef8b8e
Kirpal Singh <kayirpal@gmail.com>
2016-04-28 09:46:35 +0530

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -52,7 +52,7 @@ router.post("/", function (req, res) {
         if (error) {
 
             res.status(500);
-            res.render('error', { error: error });
+                    console.log(error);
         } else {
             res.json(user);
         }
